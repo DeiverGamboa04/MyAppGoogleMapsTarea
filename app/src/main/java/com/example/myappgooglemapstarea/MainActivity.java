e894c4470e25a5110e6655712620e27e653bb68e
DeiverGamboa04 <135407018+DeiverGamboa04@users.noreply.github.com>
2023-09-15 02:25:06 -0500

    Actualización

--- app/src/main/java/com/example/myappgooglemapstarea/MainActivity.java
+++ app/src/main/java/com/example/myappgooglemapstarea/MainActivity.java
@@ -1,66 +1,120 @@
 package com.example.myappgooglemapstarea;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.FetchPlaceRequest;
+import com.google.android.libraries.places.api.net.FetchPlaceResponse;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
 
+    private GoogleMap mMap;
+    private PlacesClient placesClient;
 
-    GoogleMap Mapa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Inicializa la API de Google Places
+        Places.initialize(getApplicationContext(), "AQUÍ_PEGA_TU_CLAVE_DE_API");
 
-        SupportMapFragment mapFragment =
-                (SupportMapFragment) getSupportFragmentManager()
-                        .findFragmentById(R.id.map);
+        // Crea una instancia de PlacesClient
+        placesClient = Places.createClient(this);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
 
         mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
 
-        Mapa = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
 
-        Mapa.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        Mapa.getUiSettings().setZoomControlsEnabled(true);
+        // Mover el mapa a una ubicación inicial (por ejemplo, Quevedo)
+        LatLng quevedo = new LatLng(-1.0113558476088707, -79.46938086135764);
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(quevedo, 15);
+        mMap.moveCamera(cameraUpdate);
 
-        //Mover el Mapa a Quevedo
-        CameraUpdate camUpd1 = CameraUpdateFactory
-                .newLatLngZoom(new
-                                LatLng(-1.0113558476088707, -79.46938086135764),
-                        15);
-        Mapa.moveCamera(camUpd1);
+        mMap.setOnMapClickListener(this);
 
-        Mapa.setOnMapClickListener(this);
+        mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+            @Override
+            public View getInfoWindow(Marker marker) {
+                return null;
+            }
+
+            @Override
+            public View getInfoContents(Marker marker) {
+                // Crear una vista personalizada para el cuadro de información del marcador
+                View infoView = getLayoutInflater().inflate(R.layout.custom_info_window, null);
+
+                // Obtén el ID del lugar desde el marcador (si lo tienes)
+                String placeId = marker.getSnippet();
+
+                if (placeId != null) {
+                    // Configura la solicitud para obtener detalles del lugar
+                    List<Place.Field> placeFields = Arrays.asList(Place.Field.NAME, Place.Field.ADDRESS);
+                    FetchPlaceRequest request = FetchPlaceRequest.newInstance(placeId, placeFields);
+
+                    // Realiza la solicitud a la API de Google Places
+                    placesClient.fetchPlace(request).addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            FetchPlaceResponse response = task.getResult();
+                            Place place = response.getPlace();
+
+                            // Actualiza la vista del cuadro de información con los datos del lugar
+                            TextView titleTextView = infoView.findViewById(R.id.text_title);
+                            TextView descriptionTextView = infoView.findViewById(R.id.text_description);
+
+                            titleTextView.setText(place.getName());
+                            descriptionTextView.setText(place.getAddress());
+                        }
+                    });
+                }
+
+                return infoView;
+            }
+        });
+
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                // Manejar el evento de clic en el cuadro de información del marcador
+                // Por ejemplo, abrir una actividad de detalle del lugar
+            }
+        });
     }
 
     @Override
     public void onMapClick(@NonNull LatLng latLng) {
+        // Agregar un marcador al lugar donde se hizo clic
+        Marker marker = mMap.addMarker(new MarkerOptions()
+                .position(latLng)
+                .title("Título del Lugar")
+                .snippet("AQUÍ_PUEDES_PONER_EL_PLACE_ID_DEL_LUGAR"));
 
-        Mapa.addMarker(new MarkerOptions().position(latLng)
-                .title("")
-                .snippet("")
-
-        );
-
-        CircleOptions circulo = new CircleOptions();
-        circulo.center(latLng);
-        circulo.radius(1500);
-        Mapa.addCircle(circulo);
+        // Mostrar el cuadro de información del marcador
+        marker.showInfoWindow();
     }
 }
